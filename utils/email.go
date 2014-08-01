28dd9f43313fd9bb8094b53869de4cc81d2e0ce4
gogames <1805601022@qq.com>
2014-08-02 00:41:50 +0800

    bug fix for smtp

--- utils/email.go
+++ utils/email.go
@@ -9,14 +9,18 @@ import (
 const emailConfTpl = `{"identity":"%s","username":"%s","password":"%s","host":"%s","port":%d,"from":"%s"}`
 
 var emailConf string
-var email = utils.NewEMail(emailConf)
+var email *utils.Email
 
 // have to set it, otherwise you can not send email
 func SetEmailConf(identity, username, password, host, from string, port int) {
 	emailConf = fmt.Sprintf(emailConfTpl, identity, username, password, host, port, from)
+	email = utils.NewEMail(emailConf)
 }
 
 func SendMail(text, subject string, to ...string) error {
+	if email == nil {
+		return fmt.Errorf("SMTP邮箱还未配置")
+	}
 	email.To = to
 	email.Subject = subject
 	email.Text = text
