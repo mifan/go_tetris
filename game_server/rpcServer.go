80798444d7029a5ee370153c29f4dfe208205b60
gogames <1805601022@qq.com>
2014-08-01 22:11:39 +0800

    log

--- game_server/rpcServer.go
+++ game_server/rpcServer.go
@@ -16,6 +16,8 @@ type stub struct{}
 type se struct{}
 
 func (se) OnBeforeInvoke(funcName string, params []reflect.Value, isSimple bool, ctx interface{}) {
+	log.Info(utils.HproseLog(funcName, params, ctx))
+
 	if ip := utils.GetIp(ctx); ip != authServerIp {
 		panic(fmt.Errorf("do not accept request from this client: %v", ip))
 	}
