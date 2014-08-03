21fd5cbaa7d4b445cab4230aef691fdf1721a429
gogames <1805601022@qq.com>
2014-08-04 00:26:08 +0800

    debug

--- auth_hall_server/pub_server.go
+++ auth_hall_server/pub_server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"reflect"
+	"time"
 
 	"github.com/gogames/go_tetris/utils"
 	"github.com/hprose/hprose-go/hprose"
@@ -18,6 +19,7 @@ type (
 )
 
 func (pubSe) OnBeforeInvoke(fName string, params []reflect.Value, isSimple bool, ctx interface{}) {
+	log.Info("%v", time.Now())
 	log.Info(utils.HproseLog(fName, params, ctx))
 	if !pubServerEnable {
 		panic("we are closing the server, not accept request at the moment")
@@ -26,7 +28,9 @@ func (pubSe) OnBeforeInvoke(fName string, params []reflect.Value, isSimple bool,
 	session.CreateSession(ctx)
 }
 
-func (pubSe) OnAfterInvoke(string, []reflect.Value, bool, []reflect.Value, interface{}) {}
+func (pubSe) OnAfterInvoke(string, []reflect.Value, bool, []reflect.Value, interface{}) {
+	log.Info("%v", time.Now())
+}
 
 func (pubSe) OnSendError(error, interface{}) {}
 
